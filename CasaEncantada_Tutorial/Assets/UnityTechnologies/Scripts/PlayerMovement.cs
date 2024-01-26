4a737002e6943343516ad024d306575874466c27
PotatoeGoat <147033284+PotatoeGoat@users.noreply.github.com>
2024-01-26 09:35:54 +0000

    005

--- CasaEncantada_Tutorial/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ CasaEncantada_Tutorial/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -6,17 +6,24 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector3 m_Movement;
 
+    Quaternion m_Rotation = Quaternion.identity;
+
     Animator m_Animator;
+
+    Rigidbody m_Rigidbody;
+
+    public float turnSpeed = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //accedo al componente Animator para crear la referencia a este componente y que otros métodos puedan acceder a él
+        //accedo al componente Animator para crear la referencia a este componente y que otras funciones puedan acceder a él
         m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         //movimiento del personaje
@@ -40,5 +47,34 @@ public class PlayerMovement : MonoBehaviour
 
         //le asigno el valor que devuelve el bool "isWalking" al parámetro bool que creé en el Animator de unity "IsWalking" que contiene las animaciones del personaje.
         m_Animator.SetBool("IsWalking", isWalking);
+
+        //ROTAR AL PERSONAJE SEGUN QUE TECLA ESTEMOS PULSANDO
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+
+        //crear y almacenar la rotación del personaje:
+        //llama a la función LookRotation y crea una rotación mirando a la dirección que le da el parámetro dado, en este caso "desiredForward"
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        
+
     }
+
+    //APLICAR EL MOVIMIENTO Y ROTACIÓN AL PERSONAJE
+
+    //declaramos una nueva función
+    private void OnAnimatorMove()
+    {
+        //llamamos a la función MovePosition y le asignamos un nuevo valor a sus parámetros
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+
+
+
+        //lo mismo, pero aplicado a la rotación, aunque esta vez no le aplicamos un cambio a los parámetros de la función; simplemente, estamos configurándola
+        m_Rigidbody.MoveRotation(m_Rotation);
+
+
+    }
+
+    
 }
